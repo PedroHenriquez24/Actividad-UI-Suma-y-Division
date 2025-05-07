5e61daa04414f950c1a57eaa20c0871657a02f94
Pedro <pedrohenriiquez.2008@gmail.com>
2025-05-07 15:43:34 -0300

    EAC Completado

---
+++ Assets/SUMADORUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SUMADORUI : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public TMP_InputField InputNum1;
+    public TMP_InputField InputNum2;
+    public TextMeshProUGUI textResultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textResultado.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SumarYMostrarResultado()
+    {
+        //Tomar los valores del input y sumarlos
+        num1 = int.Parse(InputNum1.text);
+        num2 = int.Parse(InputNum2.text);
+        textResultado.text = $"{num1 + num2}";
+    }
+    
+}
